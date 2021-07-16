027e569c20dcdc8ca1f54057fa366845d2963678
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-07-16 18:40:24 +0300

    feat:Added food category card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import React from "react";
-
+// import PropTypes from "prop-types";
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <p>Welcome to the capstone projects, this is your initial react app</p>
       </header>
-    </div>
+      </div>
   );
 }
 
