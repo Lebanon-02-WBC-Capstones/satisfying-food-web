aab59f52e60bb6bf3e5ff16d459ba19f1e4317d6
Marwa Awwad <marwaawwad10@gmail.com>
2021-07-25 22:42:48 +0300

    style: adjust the padding of the button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 import React from "react";
-import Addrecipe from "./Components/Addrecipe/Addrecipe.jsx";
+import Addrecipe from "./components/Addrecipe/Addrecipe.jsx";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Welcome to the capstone projects, this is your initial react app</p>
         <Addrecipe />
       </header>
     </div>
--- src/Components/Addrecipe/Addrecipe.jsx
+++ src/Components/Addrecipe/Addrecipe.jsx
@@ -13,11 +13,16 @@ const Addrecipe = () => {
           className=" object-cover h-full w-full"
         ></img>
       </div>
-      <div className=" col-start-3 w-20 box-content bg-red-500 text-yellow-200 font-bold h-50 w-20 p-4 rounded-r-lg">
-        <p className=" leading-none text-left  py-4">Share them with us!</p>
-        <button className="font-bold bg-yellow-200  rounded-lg text-red-500 text-base py-1 px-3">
-          Share
-        </button>
+      <div className=" col-start-3 w-20 box-content bg-red-500 font-bold h-50 w-20 p-4 rounded-r-lg">
+        <div className="flex flex-col justify-center">
+          <p className=" leading-none text-left text-yellow-200 py-4">
+            Share them with us!
+          </p>
+
+          <button className="bg-yellow-200 w-14  rounded-lg text-red-500 font-bold text-sm h-7 ">
+            Share
+          </button>
+        </div>
       </div>
     </div>
   );
