fe41454e42c1d722565b121509278cdea4be0b46
Abdallah Safar <abdallah.safar01@gmail.com>
2021-07-25 12:14:51 +0300

    Create TeamCardItem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import React from "react";
+// import PropTypes from "prop-types";
+import TeamCardItem from "./Components/team-card-item/team-card-item.jsx";
+// import Test from "./Components/team-card-item/test";
 
 function App() {
+  const person = {
+    name: "Ahmad",
+    role: "Developer",
+    imageURL: "https://image.flaticon.com/icons/png/128/2922/2922510.png"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome to the capstone projects, this is your initial react app</p>
+    <div>
+      <header>
+        <TeamCardItem person={person} />
       </header>
     </div>
   );
