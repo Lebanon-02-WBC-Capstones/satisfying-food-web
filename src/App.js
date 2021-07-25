c84a157082de084c971f3d90ed1fe31eb39df032
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-07-25 15:45:50 +0300

    removed app.css edits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import React from "react";
-// import PropTypes from "prop-types";
 function App() {
   return (
     <div>
       <header className="App-header">
         <p>Welcome to the capstone projects, this is your initial react app</p>
       </header>
-      </div>
+    </div>
   );
 }
-
 export default App;
