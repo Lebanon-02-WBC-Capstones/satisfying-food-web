b2ea4cf1d37431a9931ad34fa2fc5df764333117
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-07-25 17:55:35 +0300

    Fixed styles, added only tailwind

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -3,13 +3,17 @@ import PropTypes from "prop-types";
 import "./style.css";
 function Category(Category) {
   return (
-    <div className="rounded-lg overflow-hidden shadow-2xl" id="card">
+    <div
+      className="rounded-lg overflow-hidden shadow-2xl w-16 h-12 bg-gray-200"
+      id="card"
+    >
       <div className="img-container" id="container">
-        <img src={Category.data.Image} alt="category-icon" />
-        <div
-          className="bg-white text-xs mb-1 text-center text-yellow-500"
-          id="title"
-        >
+        <img
+          src={Category.data.Image}
+          alt="category-icon"
+          className="m-auto w-2/5"
+        />
+        <div className="text-xs mb-1 text-center text-yellow-500" id="title">
           {Category.data.Title}
         </div>
       </div>
