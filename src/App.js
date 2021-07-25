3071d2e6a665f5ce67c8f578f518aae9f9769762
Marwa Awwad <marwaawwad10@gmail.com>
2021-07-25 22:56:13 +0300

    Fix: clean up the code and fix the problems

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
 import "./App.css";
 import React from "react";
-import Addrecipe from "./components/Addrecipe/Addrecipe.jsx";
+
 import FoodItemFeatures from "./components/FoodItemFeatures/FoodItemFeatures.jsx";
 
 function App() {
-  const data1 = {
+  const Features1 = {
     Icon: "https://image.flaticon.com/icons/png/128/870/870620.png",
     Title: "250 cal",
-  };
-
-  const data = {
-    Icon: "https://image.flaticon.com/icons/png/128/3375/3375049.png",
-    Title: "3 portions",
-  };
-  const data2 = {
-    Icon: "https://image.flaticon.com/icons/png/128/2784/2784459.png",
-    Title: "45 min",
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <p></p>
-
-        <FoodItemFeatures data={data} />
-        <FoodItemFeatures data={data1} />
-        <FoodItemFeatures data={data2} />
-        <Addrecipe />
+        <FoodItemFeatures Features={Features1} />
       </header>
     </div>
   );
--- src/Components/FoodItemFeatures/FoodItemFeatures.jsx
+++ src/Components/FoodItemFeatures/FoodItemFeatures.jsx
@@ -4,16 +4,16 @@ import PropTypes from "prop-types";
 const FoodItemFeatures = (Fooditem) => {
   return (
     <div className="flex items-center  box-content bg-gray-50 border-2 border-orange-500 drop-shadow-lg text-gray-900 font-bold h-4   w-32 p-4 rounded-full ">
-      <img src={Fooditem.data.Icon} className="w-6" />
+      <img src={Fooditem.Features.Icon} className="w-6" />
       <span className="w-32 h-6 font-bold  font-body text-lg ">
-        {Fooditem.data.Title}
+        {Fooditem.Features.Title}
       </span>
     </div>
   );
 };
 
 FoodItemFeatures.PropTypes = {
-  data: PropTypes.object,
+  Features: PropTypes.object,
 };
 
 export default FoodItemFeatures;
