a3b491d0f1ca80f7244df75dc0dd7a47c63c216b
Abdallah Safar <abdallah.safar01@gmail.com>
2021-07-25 21:30:26 +0300

    Update requested changes of TeamItemCard component

---
+++ src/Components/TeamCardItem/TeamCardItem.jsx
@@ -0,0 +1,18 @@
+import React from "react" ;
+import PropTypes from "prop-types";
+function TeamCardItem(memberData) {
+    const {person} = memberData;
+    return (
+    <div className="flex flex-col items-center">
+        <img src={person.imageURL} alt={person.name} className="rounded-full mb-1 w-20 h-20" />
+        <p className="text-sm font-bold">{person.name}</p>
+        <p className="text-xs">{person.role}</p>
+    </div>
+    );
+}
+
+TeamCardItem.PropTypes = {
+    TeamCardItem:PropTypes.object
+      };
+
+export default TeamCardItem;
