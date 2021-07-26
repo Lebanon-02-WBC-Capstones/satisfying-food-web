de249cba7d4699b8239abf73cfe195d7ae753300
Marwa Awwad <marwaawwad10@gmail.com>
2021-07-26 16:06:59 +0300

    Doc: add the folder with small c

---
+++ src/components/FoodItemFeatures/FoodItemFeatures.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const FoodItemFeatures = (Fooditem) => {
+  return (
+    <div className="flex items-center  box-content bg-gray-50 border-2 border-orange-500 drop-shadow-lg text-gray-900 font-bold h-4   w-32 p-4 rounded-full ">
+      <img src={Fooditem.Features.Icon} className="w-6" />
+      <span className="w-32 h-6 font-bold  font-body text-lg ">
+        {Fooditem.Features.Title}
+      </span>
+    </div>
+  );
+};
+
+FoodItemFeatures.PropTypes = {
+  Features: PropTypes.object,
+};
+
+export default FoodItemFeatures;
