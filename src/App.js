ef25bee300494e45044880666dc1294c303a7673
osamaakb <osama98akb@gmail.com>
2021-07-26 19:18:20 +0300

    fix: remove mock data from app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
 import React from "react";
-import Footer from "./Components/Footer";
+import Footer from "./Componentss/Footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  const person = {
-    name: "Osama Abdullah",
-    role: "Leader",
-    imageURL: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  };
 
   return (
     <div>
