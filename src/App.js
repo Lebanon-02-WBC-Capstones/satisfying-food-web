7b48a25f2352a503353f620163cf324c8a19df26
osamaakb <osama98akb@gmail.com>
2021-07-26 19:46:31 +0300

    fixed: width and eslint warnings

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import React from "react";
-import Footer from "./componentss/Footer";
+import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import FoodItemFeatures from "./components/FoodItemFeatures/FoodItemFeatures.jsx";
 
 function App() {
-
+  const feat = {
+    icon: "https://image.flaticon.com/icons/png/128/870/870620.png",
+    title: "250 cal",
+  };
   return (
     <div>
       <Router>
+        <FoodItemFeatures foodItem={feat} />
         <Route path="/" exact />
         <Route path="/foodlists" />
         <Route path="/filter" />
--- src/components/FoodItemFeatures/FoodItemFeatures.jsx
+++ src/components/FoodItemFeatures/FoodItemFeatures.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const FoodItemFeatures = (Fooditem) => {
+const FoodItemFeatures = ({ foodItem }) => {
   return (
-    <div className="flex items-center justify-center box-content bg-gray-50 border-2 border-orange-500 drop-shadow-lg text-gray-900 font-bold h-4 w-full p-4 rounded-full">
-      <img src={Fooditem.Features.Icon} className="w-6" />
-      <span className="h-6 font-bold font-body text-lg text-center pl-2">
-        {Fooditem.Features.Title}
+    <div className="flex items-center justify-center box-content bg-gray-50 border-2 border-orange-500 drop-shadow-lg text-gray-900 font-bold h-4 p-4 rounded-full">
+      <img src={foodItem.icon} className="w-6" />
+      <span className="h-6 font-bold font-body text-lg text-center pl-2 text-black">
+        {foodItem.title}
       </span>
     </div>
   );
 };
 
 FoodItemFeatures.PropTypes = {
-  Features: PropTypes.object,
+  foodItem: {
+    icon: PropTypes.string,
+    title: PropTypes.string,
+  },
 };
 
 export default FoodItemFeatures;
