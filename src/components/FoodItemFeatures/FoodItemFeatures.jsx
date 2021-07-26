2efec8ba9cc32a24209585912edad20226df09f4
osamaakb <osama98akb@gmail.com>
2021-07-26 19:34:24 +0300

    fix: width

--- src/components/FoodItemFeatures/FoodItemFeatures.jsx
+++ src/components/FoodItemFeatures/FoodItemFeatures.jsx
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 
 const FoodItemFeatures = (Fooditem) => {
   return (
-    <div className="flex items-center  box-content bg-gray-50 border-2 border-orange-500 drop-shadow-lg text-gray-900 font-bold h-4   w-32 p-4 rounded-full ">
+    <div className="flex items-center justify-center box-content bg-gray-50 border-2 border-orange-500 drop-shadow-lg text-gray-900 font-bold h-4 w-full p-4 rounded-full">
       <img src={Fooditem.Features.Icon} className="w-6" />
-      <span className="w-32 h-6 font-bold  font-body text-lg ">
+      <span className="h-6 font-bold font-body text-lg text-center pl-2">
         {Fooditem.Features.Title}
       </span>
     </div>
