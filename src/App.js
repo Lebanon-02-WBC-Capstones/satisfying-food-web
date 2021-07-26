43f6e593f245dfcaf5f71f4d1373d34f1bc7e7e9
Marwa Awwad <marwaawwad10@gmail.com>
2021-07-27 01:38:22 +0300

    Feat: add contact us form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import "./App.css";
 import React from "react";
 
-import FoodItemFeatures from "./components/FoodItemFeatures/FoodItemFeatures.jsx";
+import ContactUs from "./components/ContactUs/ContactUs.jsx";
 
 function App() {
-  const Features1 = {
-    Icon: "https://image.flaticon.com/icons/png/128/870/870620.png",
-    Title: "250 cal",
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <FoodItemFeatures Features={Features1} />
+        <ContactUs />
       </header>
     </div>
   );
