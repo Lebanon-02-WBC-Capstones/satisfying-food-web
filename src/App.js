0874d98d7861f0974c88080f4710f96463c0fdfe
osamaakb <osama98akb@gmail.com>
2021-07-26 19:59:01 +0300

    fix: remove mock data from app.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,10 @@ import React from "react";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import FoodItemFeatures from "./components/FoodItemFeatures/FoodItemFeatures.jsx";
-
 function App() {
-  const feat = {
-    icon: "https://image.flaticon.com/icons/png/128/870/870620.png",
-    title: "250 cal",
-  };
   return (
     <div>
       <Router>
-        <FoodItemFeatures foodItem={feat} />
         <Route path="/" exact />
         <Route path="/foodlists" />
         <Route path="/filter" />
