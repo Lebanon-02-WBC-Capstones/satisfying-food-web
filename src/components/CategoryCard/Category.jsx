7e13801340fdece24bd219311aaa174a2c11ed8a
Adnan-961 <adnan.sayed.official@gmail.com>
2021-07-28 12:49:31 +0300

    pushing session code

--- src/components/CategoryCard/Category.jsx
+++ src/components/CategoryCard/Category.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-function Category(Category) {
+function Category(category) {
   return (
     <div className="rounded-lg overflow-hidden shadow-2xl w-20 h-12 bg-gray-300" id="card">
         <div className="img-container grid justify-items-stretch bg-gray-300" id="container">
-        <img src={Category.data.Image} alt="category-icon" className="w-2/5 flex justify-self-center" />
+        <img src={category.data.Image} alt="category-icon" className="w-2/5 flex justify-self-center" />
         <div className="bg-white text-xs mb-1 font-bold text-center text-yellow-500 bg-gray-300 flex justify-center" id="title">
-        {Category.data.Title}
+        {category.data.Title}
         </div>
         </div>
     </div>
