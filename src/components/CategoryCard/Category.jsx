3119f8e5bb7c6dc13607f932e550b7ed4eb7ab35
osamaakb <osama98akb@gmail.com>
2021-07-28 15:21:52 +0300

    Fix: clean the code and fix props issue

--- src/components/CategoryCard/Category.jsx
+++ src/components/CategoryCard/Category.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
-function Category(category) {
+function Category({ image, title }) {
   return (
-    <div className="rounded-lg overflow-hidden shadow-2xl w-20 h-12 bg-gray-300" id="card">
-        <div className="img-container grid justify-items-stretch bg-gray-300" id="container">
-        <img src={category.data.Image} alt="category-icon" className="w-2/5 flex justify-self-center" />
-        <div className="bg-white text-xs mb-1 font-bold text-center text-yellow-500 bg-gray-300 flex justify-center" id="title">
-        {category.data.Title}
-        </div>
-        </div>
-      </div>
+    <div className="rounded-lg shadow-2xl w-24 h-full bg-gray-100 py-1 m-2 hover:shadow-xl transition-all">
+      <img
+        src={image}
+        alt="category-icon"
+        className="w-full h-8 flex justify-self-center object-contain"
+      />
+      <p className="text-xs mt-1 font-semibold text-center text-yellow-500">
+        {title}
+      </p>
     </div>
   );
 }
-Category.defaultProps={
-  
-  data:{Image:"https://image.flaticon.com/icons/png/512/1548/1548784.png",
-  Title:"Not Found",
-  id: "0"
-}};
-  Category.PropTypes = {
-      data:PropTypes.object
-    };
+Category.defaultProps = {
+  image: "https://image.flaticon.com/icons/png/512/1548/1548784.png",
+  title: "Not Found",
+  id: "0",
+};
+
+Category.prototypes = {
+  image: PropTypes.string,
+  title: PropTypes.string,
+  id: PropTypes.string,
+};
+
 export default Category;
