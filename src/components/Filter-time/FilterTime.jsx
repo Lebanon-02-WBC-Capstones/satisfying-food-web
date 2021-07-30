61aefd732409e9797f1cbb30120ad5accb6f5c08
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-07-30 23:47:22 +0300

    added Filter by time component along with tailwind and click function

---
+++ src/components/Filter-time/FilterTime.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
+function FilterTime({time}) {
+  let count = 0;
+  let notClicked ="shadow-lg rounded-lg bg-gray-100 flex flex-col text-sm h-8 w-12 justify-center items-center border-solid border border-yellow-600 border-opacity-40 text-yellow-600";
+  let clicked="shadow-lg rounded-lg bg-yellow-600 flex flex-col text-sm h-8 w-12 justify-center items-center border-solid border border-yellow-700 text-white";
+  const [divClass, setClass] = useState(notClicked);
+  return (
+    <div className="mt-7 m-px" 
+    onClick={() => {
+        if (count % 2 != 0) setClass(clicked);
+        else setClass(notClicked);
+        count++;
+      }}>
+    <div className={divClass}>
+    <img src="https://image.flaticon.com/icons/png/128/714/714903.png"  className="w-8 h-8 static -mt-8 "/>
+    <span className="p-px text-xs font-semibold"> {time} Min</span>
+    </div>
+    </div>
+  );
+}
+
+FilterTime.defaultProps = {
+time:"0"
+};
+FilterTime.PropTypes = {
+  time: PropTypes.string,
+};
+export default FilterTime;
