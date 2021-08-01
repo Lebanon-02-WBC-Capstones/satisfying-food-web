0dfef9bb18bad72140b2263f3be5629f10b414a0
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-08-01 18:03:32 +0300

    final changes , bg white , padding and width/height

--- src/components/Food-item-card/Food.jsx
+++ src/components/Food-item-card/Food.jsx
@@ -16,25 +16,23 @@ function Food({ img, title, cal, time, id }) {
   }
   return (
     <div
-      className="bg-gray-300 w-40 h-56 rounded-lg shadow-2xl "
+      className="bg-white w-1/2 h-56 rounded-lg shadow-2xl m-px"
       id="main-card"
       onClick={handleClick}
     >
-      <div id="img-container" className="bg-gray-300 w-full h-36 rounded-t-lg">
+      <div id="img-container" className="bg-white w-full h-36 rounded-t-lg">
         <img src={img} className="rounded-t-lg w-full h-full" alt="" />
       </div>
-      <div id="info-container" className="flex bg-gray-300">
-        <div id="title" className="flex w-3/4 bg-gray-300">
-          <p className="bg-gray-300 w-full font-medium pt-0.5 pl-1.5">
-            {title}
-          </p>
+      <div id="info-container" className="flex bg-white">
+        <div id="title" className="flex w-3/4 bg-white">
+          <p className="bg-white w-full font-medium pt-2 pl-1.5">{title}</p>
         </div>
-        <div id="heart" className="w-1/4 bg-gray-300 flex content-center">
+        <div id="heart" className="w-1/4 bg-white flex content-center">
           <img
             src={image}
             id="fav"
             alt="Heart-icon"
-            className="w-3/5 flex justify-center content-center mx-auto pt-px"
+            className="w-3/5 flex justify-center content-center mx-auto pt-2"
             onClick={() => {
               if (count % 2 == 0)
                 setImage(
@@ -51,11 +49,11 @@ function Food({ img, title, cal, time, id }) {
       </div>
       <div
         id="info"
-        className="flex bg-gray-300 w-full h-12 justify-center items-center"
+        className="flex bg-white w-full h-12 justify-center items-center"
       >
         <div
           id="fire"
-          className="flex h-full w-1/4 bg-gray-300 justify-center items-center pr-3 pt-2"
+          className="flex h-full w-1/4 bg-white justify-center items-center pr-3 pt-2"
         >
           <img
             src="https://image.flaticon.com/icons/png/128/870/870620.png"
@@ -64,13 +62,13 @@ function Food({ img, title, cal, time, id }) {
         </div>
         <div
           id="cal"
-          className="flex h-full w-1/4 bg-gray-300 justify-center items-center pr-3.5 pt-2 text-sm whitespace-nowrap font-medium"
+          className="flex h-full w-1/4 bg-white justify-center items-center pr-3.5 pt-2 text-sm whitespace-nowrap font-medium"
         >
           <p>{cal} Cal</p>
         </div>
         <div
           id="clocl"
-          className="flex h-full w-1/4 bg-gray-300 justify-center items-center pr-1 pt-2"
+          className="flex h-full w-1/4 bg-white justify-center items-center pr-1 pt-2"
         >
           <img
             src="https://image.flaticon.com/icons/png/128/2784/2784459.png"
@@ -79,7 +77,7 @@ function Food({ img, title, cal, time, id }) {
         </div>
         <div
           id="time"
-          className="flex h-full w-1/4 bg-gray-300 justify-center items-center pr-3 pt-2 text-sm whitespace-nowrap font-medium"
+          className="flex h-full w-1/4 bg-white justify-center items-center pr-3 pt-2 text-sm whitespace-nowrap font-medium"
         >
           <p>{time} Min</p>
         </div>
--- src/components/WhoWeAre/WhoWeAre.jsx
+++ src/components/WhoWeAre/WhoWeAre.jsx
@@ -4,12 +4,13 @@ import lap from "./laptop.svg";
 function WhoWeAre() {
   return (
     <div className="flex flex-col items-center w-96 mt-5">
-      <img
-        src={lap}
-        alt="Laptop"
-      />
-      <p className="text-2xl font-bold text-center text-yellow-500 mt-2">Who we are?</p>
-      <p className="text-md text-center mt-2">We are a team of 7 developers, who are passionate about food!</p>
+      <img src={lap} alt="Laptop" />
+      <p className="text-2xl font-bold text-center text-yellow-500 mt-2">
+        Who we are?
+      </p>
+      <p className="text-md text-center mt-2">
+        We are a team of 7 developers, who are passionate about food!
+      </p>
     </div>
   );
 }
