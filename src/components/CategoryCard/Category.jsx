cee46a25aec207da68c6318e68b1e581b020bd76
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-08-05 02:39:47 +0300

    first commit, adding files, modifed category,food-item, created Page compo

--- src/components/CategoryCard/Category.jsx
+++ src/components/CategoryCard/Category.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 function Category({ image, title }) {
   return (
-    <div className="rounded-lg shadow-2xl w-24 h-full bg-gray-100 py-1 m-2 hover:shadow-xl transition-all">
+    <div className="rounded-lg shadow-2xl w-24 h-full bg-gray-100 py-1 m-2 hover:shadow-xl transition-all flex-none content-around">
       <img
         src={image}
         alt="category-icon"
         className="w-full h-8 flex justify-self-center object-contain"
       />
-      <p className="text-xs mt-1 font-semibold text-center text-yellow-500">
+      <p className="text-sm flex justify-center align-center font-semibold text-center text-yellow-500">
         {title}
       </p>
     </div>
