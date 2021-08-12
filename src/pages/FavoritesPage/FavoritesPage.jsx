01b0c1a5662c9cef356812c6b437d1f986cca218
Marwa Awwad <marwaawwad10@gmail.com>
2021-08-12 21:56:46 +0300

    Fix: fix the code and remove the typo

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,33 +1,31 @@
 import { React } from "react";
-import Food from "../../Food-item-card/Food";
+import Food from "../../components/Food-item-card/Food";
 
 function FavoritesPage() {
-const storage = window.localStorage;
-var fav = JSON.parse(storage.getItem("fav")) ? JSON.parse(storage.getItem("fav")) : [];
-
-   const Foods = fav.map((item) => (
-
-    <Foodd
-      key={item.title}
-      id={item.id}
-      img={item.img}
-      title={item.title}
-      time={item.time}
-      cal={item.cal}
-    />
-  ));
+  const storage = window.localStorage;
+  const fav = JSON.parse(storage.getItem("fav"))
+    ? JSON.parse(storage.getItem("fav"))
+    : [];
 
   return (
     <div>
       <div className=" w-full h-42 p-2">
         <h1 className="text-orange-500 font-bold font-body py-8 px-8 text-4xl">
-         
           Favorites
         </h1>
 
         <div id="align-food-div-center" className="flex align-center">
           <div className="w-full flex flex-wrap content-around justify-center ">
-            {Foods}
+            {fav.map((item) => (
+              <Food
+                key={item.title}
+                id={item.id}
+                img={item.img}
+                title={item.title}
+                time={item.time}
+                cal={item.cal}
+              />
+            ))}
           </div>
         </div>
       </div>
