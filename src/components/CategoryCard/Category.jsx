6a67bf7e032800ae8d7a25188a9963ed1bddbeb5
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-08-12 17:00:27 +0300

    Added on category click feature to change color

--- src/components/CategoryCard/Category.jsx
+++ src/components/CategoryCard/Category.jsx
@@ -1,14 +1,24 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import PropTypes from "prop-types";
+import {useState} from "react";
 function Category({ image, title }) {
+  let count=0;
+  let notClicked= "rounded-lg shadow-2xl w-24 h-full text-yellow-500 bg-gray-100 py-1 m-2 hover:shadow-xl transition-all";
+  let clicked="rounded-lg shadow-2xl w-24 text-white h-full bg-yellow-500 py-1 m-2 hover:shadow-xl transition-all ";
+  const [catStyle,setCatStyle]= useState(notClicked);
   return (
-    <div className="rounded-lg shadow-2xl w-24 h-full bg-gray-100 py-1 m-2 hover:shadow-xl transition-all">
+    <div className={catStyle} onClick={() => {
+      if (count % 2 != 0) setCatStyle(clicked);
+      else setCatStyle(notClicked);
+      count++;
+    }}>
       <img
         src={image}
         alt="category-icon"
         className="w-full h-8 flex justify-self-center object-contain"
       />
-      <p className="text-xs mt-1 font-semibold text-center text-yellow-500">
+      <p className="text-xs mt-1 font-semibold text-center">
         {title}
       </p>
     </div>
