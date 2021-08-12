ddd4098df129364b82abcd719878c5e3187bddb2
Marwa Awwad <marwaawwad10@gmail.com>
2021-08-12 21:57:47 +0300

    the food item card format I worked on

--- src/components/Food-item-card/Food.jsx
+++ src/components/Food-item-card/Food.jsx
@@ -3,7 +3,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
-function Food({ img, title, cal, time, id }) {
+function Foodd({ img, title, cal, time, id, categories, ingredients }) {
+  const storage = window.localStorage;
+  var fav = JSON.parse(storage.getItem("fav"))
+    ? JSON.parse(storage.getItem("fav"))
+    : [];
+  const currentItem = {
+    title: title,
+    img: img,
+    cal: cal,
+    time: time,
+    id: id,
+    categories: categories,
+    ingredients: ingredients,
+  };
+  const removeItem = (array, item) => {
+    const index = array.findIndex(
+      (element) => JSON.stringify(element) == JSON.stringify(item)
+    );
+    if (index != -1) {
+      array.splice(index, 1);
+      return array;
+    }
+  };
   let count = 0;
   const [image, setImage] = useState(
     "https://image.flaticon.com/icons/png/128/833/833300.png"
@@ -16,14 +38,14 @@ function Food({ img, title, cal, time, id }) {
   }
   return (
     <div
-      className="bg-white w-1/2 h-56 rounded-lg shadow-2xl m-px"
+      className="bg-white w-40 h-56 rounded-lg shadow-2xl mt-6 mr-3 ml-3"
       id="main-card"
       onClick={handleClick}
     >
       <div id="img-container" className="bg-white w-full h-36 rounded-t-lg">
         <img src={img} className="rounded-t-lg w-full h-full" alt="" />
       </div>
-      <div id="info-container" className="flex bg-white">
+      <div id="info-container" className="flex bg-white  ">
         <div id="title" className="flex w-3/4 bg-white">
           <p className="bg-white w-full font-medium pt-2 pl-1.5">{title}</p>
         </div>
@@ -34,50 +56,64 @@ function Food({ img, title, cal, time, id }) {
             alt="Heart-icon"
             className="w-3/5 flex justify-center content-center mx-auto pt-2"
             onClick={() => {
-              if (count % 2 == 0)
+              if (count % 2 == 0) {
                 setImage(
                   "https://image.flaticon.com/icons/png/128/833/833300.png"
                 );
-              else
+
+                fav = JSON.parse(storage.getItem("fav"))
+                  ? JSON.parse(storage.getItem("fav"))
+                  : [];
+
+                removeItem(fav, currentItem);
+                storage.setItem("fav", JSON.stringify(fav));
+              } else {
                 setImage(
                   "https://image.flaticon.com/icons/png/128/833/833472.png"
                 );
+                fav.push(currentItem);
+                storage.setItem("fav", JSON.stringify(fav));
+              }
               count++;
+              fav = JSON.parse(storage.getItem("fav"))
+                ? JSON.parse(storage.getItem("fav"))
+                : [];
+              //console.log(fav);
             }}
           />
         </div>
       </div>
       <div
         id="info"
-        className="flex bg-white w-full h-12 justify-center items-center"
+        className="flex bg-white w-full h-12 justify-center items-center rounded-lg"
       >
         <div
           id="fire"
-          className="flex h-full w-1/4 bg-white justify-center items-center pr-3 pt-2"
+          className="flex h-full w-1/4 bg-white justify-center items-center rounded-lg pr-3 pt-2"
         >
           <img
             src="https://image.flaticon.com/icons/png/128/870/870620.png"
-            className="w-3/5 justify-center items-center"
+            className="w-3/5 justify-center items-center rounded-lg"
           />
         </div>
         <div
           id="cal"
-          className="flex h-full w-1/4 bg-white justify-center items-center pr-3.5 pt-2 text-sm whitespace-nowrap font-medium"
+          className="rounded-lg flex h-full w-1/4 bg-white justify-center items-center pr-3.5 pt-2 text-sm whitespace-nowrap font-medium"
         >
           <p>{cal} Cal</p>
         </div>
         <div
           id="clocl"
-          className="flex h-full w-1/4 bg-white justify-center items-center pr-1 pt-2"
+          className="rounded-lg flex h-full w-1/4 bg-white justify-center items-center pr-1 pt-2"
         >
           <img
             src="https://image.flaticon.com/icons/png/128/2784/2784459.png"
-            className="w-3/5 justify-center items-center pr-1"
+            className="rounded-lg w-3/5 justify-center items-center pr-1"
           />
         </div>
         <div
           id="time"
-          className="flex h-full w-1/4 bg-white justify-center items-center pr-3 pt-2 text-sm whitespace-nowrap font-medium"
+          className="rounded-lg flex h-full w-1/4 bg-white justify-center items-center pr-3 pt-2 text-sm whitespace-nowrap font-medium"
         >
           <p>{time} Min</p>
         </div>
@@ -85,18 +121,18 @@ function Food({ img, title, cal, time, id }) {
     </div>
   );
 }
-Food.defaultProps = {
+Foodd.defaultProps = {
   img: "https://image.flaticon.com/icons/png/512/1548/1548784.png",
   title: "Error",
   cal: "0",
   time: "0",
   id: "0",
 };
-Food.prototypes = {
+Foodd.prototypes = {
   img: PropTypes.string,
   title: PropTypes.string,
   id: PropTypes.string,
   cal: PropTypes.string,
   time: PropTypes.string,
 };
-export default Food;
+export default Foodd;
