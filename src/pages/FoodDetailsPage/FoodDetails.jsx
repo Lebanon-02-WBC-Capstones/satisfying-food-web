38abe33dbb8223a7c0b973901840ecb61d64dbb5
Abdallah Safar <abdallah.safar01@gmail.com>
2021-08-13 14:10:10 +0300

    feat: Add useParams to get food id, add history go back on click of back button

--- src/pages/FoodDetailsPage/FoodDetails.jsx
+++ src/pages/FoodDetailsPage/FoodDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { useParams } from "react-router";
+import { useParams } from "react-router";
 import back from "./backward.svg";
 import fire from "../../components/PopularFoodItemCard/fire.svg";
 import time from "../../components/PopularFoodItemCard/time.svg";
@@ -7,49 +7,56 @@ import portion from "./portion.svg";
 import Category from "../../components/CategoryCard/Category";
 import Food from "../../components/Food-item-card/Food";
 import * as foodData from "../../foodData";
+import { useHistory } from "react-router-dom";
 
 function FoodDetails() {
+  const history = useHistory();
 
-  // const {id} = useParams();
-  const id = "99";
+  const { id } = useParams();
+  console.log(id);
   const currentFood = foodData.FoodArray.find(
     (currentFood) => currentFood.id === id
   );
   const similarRecipes = foodData.FoodArray.filter(
     (item) => item.category[0] === currentFood.category[0]
   );
-  
-    
+
   const favArrayContains = (food) => {
-    const favArray = JSON.parse(localStorage.getItem("fav")) ? JSON.parse(window.localStorage.getItem("fav")) : [];
-    return favArray.find(item => item.id === food.id) ? true : false;
+    const favArray = JSON.parse(localStorage.getItem("fav"))
+      ? JSON.parse(window.localStorage.getItem("fav"))
+      : [];
+    return favArray.find((item) => item.id === food.id) ? true : false;
   };
 
   const [favorite, setFavorite] = useState(favArrayContains(currentFood));
-  const [heartImage,setHeartImage] = useState(favorite ? "https://image.flaticon.com/icons/png/128/833/833472.png" : "https://image.flaticon.com/icons/png/128/833/833300.png");
-  
+  const [heartImage, setHeartImage] = useState(
+    favorite
+      ? "https://image.flaticon.com/icons/png/128/833/833472.png"
+      : "https://image.flaticon.com/icons/png/128/833/833300.png"
+  );
 
-  const removeItem = (array,item) => {
+  const removeItem = (array, item) => {
     const index = array.findIndex((element) => element.id === item.id);
-    if(index != -1){
-      array.splice(index,1);
+    if (index != -1) {
+      array.splice(index, 1);
       return array;
     }
   };
 
   const handleHeartClick = () => {
-    const favArray = JSON.parse(window.localStorage.getItem("fav")) ? JSON.parse(window.localStorage.getItem("fav")) : [];
-    if(!favorite) {
-        setHeartImage("https://image.flaticon.com/icons/png/128/833/833472.png");
-        setFavorite(true);
-        favArray.push(currentFood);
-        localStorage.setItem("fav", JSON.stringify(favArray));
-    }
-    else {
-        setHeartImage("https://image.flaticon.com/icons/png/128/833/833300.png");
-        setFavorite(false);
-        removeItem(favArray, currentFood);
-        localStorage.setItem("fav", JSON.stringify(favArray));
+    const favArray = JSON.parse(window.localStorage.getItem("fav"))
+      ? JSON.parse(window.localStorage.getItem("fav"))
+      : [];
+    if (!favorite) {
+      setHeartImage("https://image.flaticon.com/icons/png/128/833/833472.png");
+      setFavorite(true);
+      favArray.push(currentFood);
+      localStorage.setItem("fav", JSON.stringify(favArray));
+    } else {
+      setHeartImage("https://image.flaticon.com/icons/png/128/833/833300.png");
+      setFavorite(false);
+      removeItem(favArray, currentFood);
+      localStorage.setItem("fav", JSON.stringify(favArray));
     }
   };
 
@@ -65,6 +72,7 @@ function FoodDetails() {
           src={back}
           alt=""
           className="absolute top-px left-px w-5 h-5 ml-1 mt-3"
+          onClick={() => history.goBack()}
         />
         <img
           src={heartImage}
@@ -93,27 +101,24 @@ function FoodDetails() {
       <div>
         <h1 className="text-2xl font-semibold ml-4 mt-3">Categories</h1>
         <div className="m-4 ml-2 mb-8 flex flex-wrap">
-            {currentFood.category.map(category => <Category
-            key = {category}
-            image={
-              foodData.categoryArray.find(
-                (item) => item.title === category
-              ).image
-            }
-            title={category}
-          />)}
+          {currentFood.category.map((category) => (
+            <Category
+              key={category}
+              image={
+                foodData.categoryArray.find((item) => item.title === category)
+                  .image
+              }
+              title={category}
+            />
+          ))}
         </div>
       </div>
       <div>
         <h1 className="text-2xl font-semibold ml-4 mt-3">Ingredients</h1>
         <div className="m-4 ml-2 mb-8 flex flex-wrap">
-        {currentFood.ingredients.map(ing => <Category
-            key = {ing}
-            image={
-              portion
-            }
-            title={ing}
-          />)}
+          {currentFood.ingredients.map((ing) => (
+            <Category key={ing} image={portion} title={ing} />
+          ))}
         </div>
       </div>
       <div className="mb-8">
@@ -137,20 +142,21 @@ function FoodDetails() {
         <h1 className="text-2xl font-semibold ml-4 mt-3">Similar Recipes</h1>
         <div className="w-full flex overflow-auto pb-4 ml-1">
           {similarRecipes.map((rec) => {
-            if(rec.title !== currentFood.title)
-            {
-                return <Food
-                key={rec.title}
-                id={rec.id}
-                img={rec.img}
-                title={rec.title}
-                time={rec.time}
-                cal={rec.cal}
-                category={rec.category[0]}
-                ingredients={rec.ingredients}
-              />;
+            if (rec.title !== currentFood.title) {
+              return (
+                <Food
+                  key={rec.title}
+                  id={rec.id}
+                  img={rec.img}
+                  title={rec.title}
+                  time={rec.time}
+                  cal={rec.cal}
+                  category={rec.category[0]}
+                  ingredients={rec.ingredients}
+                />
+              );
             }
-        })}
+          })}
         </div>
       </div>
     </div>
