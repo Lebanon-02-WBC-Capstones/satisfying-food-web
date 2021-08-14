b08b355175f840336439c8ab4769290efdbafe08
Abdallah Safar <abdallah.safar01@gmail.com>
2021-08-14 22:52:10 +0300

    fix: remove conflict

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { StateContext } from "./StateProvider";
 import Menu from "./components/Menu/Menu";
 import Navbar from "./components/Menu/Navbar";
+import FoodPage from "../src/pages/FoodItemsPage/FoodPage";
 import AboutUs from "./pages/AboutUs/AboutUs";
-
 function App() {
   const [state] = useContext(StateContext);
   let menDisp = state.menuDisp;
@@ -23,6 +23,7 @@ function App() {
             <Route path="/favorites" />
             <Route path="/signin" />
             {menDisp ? <Menu /> : <Navbar />}
+            <Route path="/foods" component={FoodPage} />
           </div>
         </Switch>
         <Footer />
