eb5f87c8caac47c7323fe7069b49b1f553f3a939
Adnan-961 <adnan.sayed.official@gmail.com>
2021-08-15 20:04:58 +0300

    resolving conflicts

--- src/components/CategoryCard/Category.jsx
+++ src/components/CategoryCard/Category.jsx
@@ -2,23 +2,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {useState} from "react";
-function Category({ image, title }) {
+function Category({ image, title, cat, getCat }) {
   let count=0;
   let notClicked= "rounded-lg shadow-2xl w-24 h-full text-yellow-500 bg-gray-100 py-1 m-2 hover:shadow-xl transition-all";
   let clicked="rounded-lg shadow-2xl w-24 text-white h-full bg-yellow-500 py-1 m-2 hover:shadow-xl transition-all ";
   const [catStyle,setCatStyle]= useState(notClicked);
   return (
-    <div className={catStyle} onClick={() => {
-      if (count % 2 != 0) setCatStyle(clicked);
-      else setCatStyle(notClicked);
-      count++;
-    }}>
+    <div
+      className={catStyle} onClick={() => {
+        if (count % 2 != 0) setCatStyle(clicked);
+        else setCatStyle(notClicked);
+        count++;
+      }}>
       <img
         src={image}
         alt="category-icon"
         className="w-full h-8 flex justify-self-center object-contain"
+        name={cat}
       />
-      <p className="text-xs mt-1 font-semibold text-center">
+      <p className="text-sm flex justify-center align-center font-semibold text-center">
         {title}
       </p>
     </div>
