1357c42d886c18cf60633c7f277cbe2b3214fc30
Abdallah Safar <62112778+AbdallahSafar@users.noreply.github.com>
2021-08-16 11:02:25 +0300

    feat: add logo to navbar, fix: fix /foods instead of /foodlists in footer (#59)

--- src/components/Menu/Navbar.jsx
+++ src/components/Menu/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
   return (
     <div className={"w-full h-12 bg-yellow-500 text-left flex items-center"}>
-      <div className={"pt-1 pl-4 flex w-24 justify-between items-center"}>
+      <div className={"pt-1 pl-3 flex w-full justify-between items-center"}>
         <div
           className="border-t-4 border-b-4 border-white w-8 h-7 pt-2 pb-2"
           onClick={change}
@@ -20,7 +20,12 @@ const Navbar = () => {
           <h1 className="border-t-4 border-white w-8"></h1>
         </div>
         <Link to="/">
-          <img src={Bu} alt="Burger-menu-icon" className="h-8" />
+          <div className="flex w-44 cursor-pointer">
+            <h1 className="font-semibold text-lg text-white mr-2">
+              Satisfying Food
+            </h1>
+            <img src={Bu} alt="Burger-menu-icon" className="h-8" />
+          </div>
         </Link>
       </div>
     </div>
