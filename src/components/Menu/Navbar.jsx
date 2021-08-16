9f1d5c5a1a1738b26669bdc43d5c99af410ebed3
osamaakb <osama98akb@gmail.com>
2021-08-16 12:33:41 +0300

    fix navbar icon

--- src/components/Menu/Navbar.jsx
+++ src/components/Menu/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Bu from "../../images/burger.svg";
 import { StateContext } from "../../StateProvider";
+import Menu from "../../images/menu.svg";
 
 const Navbar = () => {
   const [state, dispatch] = useContext(StateContext);
@@ -11,17 +12,12 @@ const Navbar = () => {
     dispatch({ type: "SET_MENU", menuDisp: true });
   };
   return (
-    <div className={"w-full h-12 bg-yellow-500 text-left flex items-center"}>
-      <div className={"pt-1 pl-3 flex w-full justify-between items-center"}>
-        <div
-          className="border-t-4 border-b-4 border-white w-8 h-7 pt-2 pb-2"
-          onClick={change}
-        >
-          <h1 className="border-t-4 border-white w-8"></h1>
-        </div>
+    <div className="w-full h-12 bg-yellow-500 text-left flex items-center">
+      <div className="pt-1 pl-3 flex w-full justify-between items-center">
+        <img src={Menu} alt="menu" onClick={change} height={24} width={24} />
         <Link to="/">
           <div className="flex w-44 cursor-pointer">
-            <h1 className="font-semibold text-lg text-white mr-2">
+            <h1 className="font-semibold text-lg text-white mr-2 pt-1">
               Satisfying Food
             </h1>
             <img src={Bu} alt="Burger-menu-icon" className="h-8" />
