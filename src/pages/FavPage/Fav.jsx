1db8b716c2780fd9c18339253ca3cd4d91ac17e5
Adnan-961 <adnan.sayed.official@gmail.com>
2021-08-17 20:39:04 +0300

    Re uploading

---
+++ src/pages/FavPage/Fav.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+import { React } from "react";
+import Food from "../../components/Food-item-card/Food";
+import {useState} from "react";
+function FavoritesPage() {
+
+  const storage = window.localStorage;
+  const fav = JSON.parse(storage.getItem("fav"))
+    ? JSON.parse(storage.getItem("fav"))
+    : [];
+let favArray =fav.map((item) => (
+  <Food
+    key={item.title}
+    id={item.id}
+    img={item.img}
+    title={item.title}
+    time={item.time}
+    cal={item.cal}
+  />
+));
+  return (
+    <div>
+      <div className="w-full h-full p-2">
+        <h1 className="text-orange-500 font-bold font-body py-8 px-8 text-4xl">
+          Favorites
+        </h1>
+
+        <div id="align-food-div-center" className="flex flex-wrap -m-2">ls
+          
+        {favArray}
+        
+        </div>
+      </div>
+        
+      </div>
+  
+  );
+}
+
+export default FavoritesPage;
